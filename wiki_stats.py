1753cc8d5a88990e38922cc9095caa9750ab4ff2
Almouro <contact@almouro.com>
2014-12-31 15:03:32 +0100

    Python script modified -> organized functions in a class

--- wiki_stats.py
+++ wiki_stats.py
@@ -1,55 +1,55 @@
 import os
 
-def get_nodes():
-	with open("result") as thefile:
-	    lines = thefile.read().splitlines()
+class WikiStats(object):
+	@staticmethod
+	def get_nodes(datafile):
+		with open(datafile) as thefile:
+		    lines = thefile.read().splitlines()
 
-	nodes = {}
+		nodes = {}
 
-	for line in lines:
-	    split = line.split('\t')
-	    name = split[0]
-	    if len(name):
-	    	name = name[0].upper() + name[1:]
-	    values = split[1:]
-	    if name in nodes:
-	    	nodes[name].extend(values)
-	    else: nodes[name] = values
+		for line in lines:
+		    split = line.split('\t')
+		    name = split[0]
+		    if len(name):
+		    	name = name[0].upper() + name[1:]
+		    values = split[1:]
+		    if name in nodes:
+		    	nodes[name].extend(values)
+		    else: nodes[name] = values
 
-	return nodes
+		return nodes
 
-NODES = get_nodes()
+	def __init__(self, datafile):
+		self.nodes = WikiStats.get_nodes(datafile)
 
-def get_next_page_for(page):
-	nc = 0
-	result = None
-	for node,children in NODES.items():
-		if page in children:
-			result = node
-			nc += 1
-	print nc
-	return result
+	def get_next_page_for(self,page):
+		for node,children in self.nodes.items():
+			if page in children:
+				return node
 
-def get_next_page_series(page, series = [], stopAt = 'Philosophy'):
-	if(page in series):
+	def get_next_page_series(self, page, series = [], stopAt = 'Philosophy'):
+		if(page in series):
+			series.append(page)
+			return series
 		series.append(page)
-		return series
-	series.append(page)
-	if(page == stopAt):
-		return series
+		if(page == stopAt):
+			return series
 
-	page = get_next_page_for(page)
-	return get_next_page_series(page, series, stopAt)
+		page = self.get_next_page_for(page)
+
+		return self.get_next_page_series(page, series, stopAt)
 
 
-def get_distance_to_page(page='Philosophy'):
-	philo_distance = {}
-	def process_node(name, distance):
-		philo_distance[name] = distance
-		if name in NODES:
-			for child in NODES[name]:
-				process_node(child, distance + 1)
+	def get_distances_to_page(self,page='Philosophy'):
+		distances = {}
 
-	process_node(page, 0)
+		def process_node(name, distance):
+			distances[name] = distance
+			if name in self.nodes:
+				for child in self.nodes[name]:
+					process_node(child, distance + 1)
 
-	return philo_distance
+		process_node(page, 0)
+
+		return distances
