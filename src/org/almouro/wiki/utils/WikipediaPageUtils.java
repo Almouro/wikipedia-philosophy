e21a7ddd41d4c9b809aca62486d4e52e32b33630
AlexMoureaux <contact@almouro.com>
2015-01-18 20:19:10 +0100

    Added method to write page files in a specified folder

--- src/org/almouro/wiki/utils/WikipediaPageUtils.java
+++ src/org/almouro/wiki/utils/WikipediaPageUtils.java
@@ -1,48 +1,78 @@
 package org.almouro.wiki.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WikipediaPageUtils {
 
-    private static String[] MEDIA_PREFIX = new String[] { "File", "Image" };
+	public static final String LINE_BREAK = System.getProperty("line.separator");
 
-    // not exhaustive -> Easier to check list of wikipedia pages
-    // TODO WIKIPEDIA_NAMESPACE_PREFIX
-    /*
-     * private static String[] WIKIPEDIA_NAMESPACE_PREFIX = new String[]
-     * {"Talk", "User", "Wikipedia", "MediaWiki", "Template", "Help",
-     * "Category", "Portal", "Book", "Draft", "Education Program", "TimedText",
-     * "Module", "Topic"};
-     */
+	private static String[] MEDIA_PREFIX = new String[] { "File", "Image" };
 
-    public static String getNextPage(String pageText) {
-	return getFormattedTitle(new NextPageGetter().readAndGetLink(pageText));
-    }
+	// not exhaustive -> Easier to check list of wikipedia pages
+	// TODO WIKIPEDIA_NAMESPACE_PREFIX
+	/*
+	 * private static String[] WIKIPEDIA_NAMESPACE_PREFIX = new String[]
+	 * {"Talk", "User", "Wikipedia", "MediaWiki", "Template", "Help",
+	 * "Category", "Portal", "Book", "Draft", "Education Program", "TimedText",
+	 * "Module", "Topic"};
+	 */
 
-    private static boolean doesTitleStartWithWikiPrefix(String title,
-	    String prefix) {
-	String pref = prefix + ":";
-	return title.startsWith(pref) || title.startsWith(":" + pref);
-    }
-
-    public static boolean isMediaPage(String title) {
-	for (String mediaPrefix : MEDIA_PREFIX) {
-	    if (doesTitleStartWithWikiPrefix(title, mediaPrefix))
-		return true;
+	public static String getNextPage(String pageText) {
+		return getFormattedTitle(new NextPageGetter().readAndGetLink(pageText));
 	}
-	return false;
-    }
 
-    /*
-     * 1. Ignore # links
-     * 
-     * 2. The MediaWiki software is configured so that a page title (as stored
-     * in the database) cannot begin with a lower-case letter, and links that
-     * begin with a lower-case letter are treated as if capitalized, i.e.
-     * [[foo]] is treated the same as [[Foo]].
-     */
-    public static String getFormattedTitle(String str) {
-	str = str.split("#")[0];
+	private static boolean doesTitleStartWithWikiPrefix(String title, String prefix) {
+		String pref = prefix + ":";
+		return title.startsWith(pref) || title.startsWith(":" + pref);
+	}
 
-	return str.length() > 0 ? str.substring(0, 1).toUpperCase()
-		+ str.substring(1) : "";
-    }
+	public static boolean isMediaPage(String title) {
+		for (String mediaPrefix : MEDIA_PREFIX) {
+			if (doesTitleStartWithWikiPrefix(title, mediaPrefix))
+				return true;
+		}
+		return false;
+	}
+
+	/*
+	 * 1. Ignore # links
+	 * 
+	 * 2. The MediaWiki software is configured so that a page title (as stored
+	 * in the database) cannot begin with a lower-case letter, and links that
+	 * begin with a lower-case letter are treated as if capitalized, i.e.
+	 * [[foo]] is treated the same as [[Foo]].
+	 */
+	public static String getFormattedTitle(String str) {
+		String[] split = str.split("#");
+		if (split.length == 0)
+			return str;
+
+		str = split[0];
+
+		return str.length() > 0 ? str.substring(0, 1).toUpperCase() + str.substring(1) : "";
+	}
+
+	/**
+	 * For debugging purposes, output all pages in a folder
+	 * 
+	 * @param outputFolder
+	 * @param pageTitle
+	 * @param pageText
+	 * @param pageNumber
+	 * @throws IOException
+	 */
+	public static void writePageToFile(String outputFolder, String pageTitle, String pageText,
+			long pageNumber) throws IOException {
+		
+		FileOutputStream fs = new FileOutputStream(new File(outputFolder
+				+ pageTitle.replaceAll("[:\\\\/*\"?|<>]", "_") + "_page" + pageNumber + ".txt"));
+
+		fs.write(pageTitle.getBytes());
+		fs.write((LINE_BREAK + LINE_BREAK).getBytes());
+		fs.write(pageText.getBytes());
+
+		fs.close();
+	}
 }
